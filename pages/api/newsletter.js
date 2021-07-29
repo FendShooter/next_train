08ce4e830d8b34340232c3063567ea0da4368c5f
FendShooter <virgile.dokouvi@gmail.com>
2021-07-28 20:35:00 -0400

    with  mail options

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -3,6 +3,9 @@ import fs from 'fs';
 import Note from '../../models/Note';
 import db_connect from '../../helper/db_connect';
 import sendEmail from '../../helper/nodemailer';
+import nodemailerSendgrid from 'nodemailer-sendgrid';
+import nodemailer from 'nodemailer';
+
 db_connect();
 
 export const getData = () => {
@@ -21,27 +24,58 @@ async function handler(req, res) {
   if (req.method === 'POST') {
     const note = await new Note(req.body);
 
-    const options = {
-      html: `<table style="width:100%; cellpadding:0; cellspacing:0">
-      <tbody>
-      <tr>
-      <td colspan="2" style="background:red; height:80px;">
-      </td>
-      </tr>
-      <tr>
-      <td>on</td>
-      <td>two</td>
-      </tr>
-      </tbody>
+  //   const options = {
+  //     html: `<table style="width:100%; cellpadding:0; cellspacing:0">
+  //     <tbody>
+  //     <tr>
+  //     <td colspan="2" style="background:red; height:80px;">
+  //     </td>
+  //     </tr>
+  //     <tr>
+  //     <td>on</td>
+  //     <td>two</td>
+  //     </tr>
+  //     </tbody>
 
-      </table>
-      `,
-    };
-    await sendEmail(options);
-    await note.save();
+  //     </table>
+  //     `,
+  //   };
+  //   await sendEmail(options);
+  //   await note.save();
 
-    res.status(201).json({ success: true, note });
-  }
+  //   res.status(201).json({ success: true, note });
+  // }
+    
+    const transport = nodemailer.createTransport(
+  nodemailerSendgrid({
+    apiKey:
+      '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',
+  })
+);
+
+transport
+  .sendMail({
+    from: 'oldhumblelion@gmail.com',
+    from: 'virgile.dokouvi@gmail.com',
+    subject: 'hello world from me',
+    html: `<h1 style="font-weight: bold;margin-bottom: 15px; font-size: 16px;">Client current location</h1>
+<hr>
+<h2 style="font-weight: bold;margin-bottom: 15px; font-size: 16px;">Ref: # <strong></strong></h1>
+<h1>Current Location:</h1>
+<div>Address From: <span></span> <span></span></div>
+<div>House: <span></span> </div>
+<div>Appartement: <span></span> </div>
+<div>Address To: <span></span> <span></span></div>
+<div>Date: <span></span> </div>
+
+</div> `,
+  })
+  .then(() => {
+    console.log('sent');
+   note.save();
+
+  //   res.status(201).json({ success: true, note });
+  });
 }
 
 export default handler;

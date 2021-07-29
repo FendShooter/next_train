055928c38a2817fbccc651904c2883e7edb8c304
FendShooter <virgile.dokouvi@gmail.com>
2021-07-28 21:09:48 -0400

    with sendgrid fixeds

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -30,7 +30,6 @@ async function handler(req, res) {
       from: 'gildryx8@yahoo.fr', // Change to your recipient
       to: 'oldhumblelion@gmail.com', // Change to your verified sender
       subject: 'Sending with SendGrid is Fun',
-      text: 'and easy to do anywhere, even with Node.js',
       html: '<strong>and easy to do anywhere, even with Node.js</strong>',
     };
     sgMail

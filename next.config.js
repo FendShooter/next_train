f2d05f9e11dbb7050c0f33ed109688316f88b3a0
Kodjo Dokouvi <virgile.dokouvi@gmail.com>
2022-03-03 13:54:35 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  env: {
-    CONNECTION_URL: `mongodb+srv://pat123:pat123@cluster0.nqxrk.mongodb.net/newsletter?retryWrites=true &
-w=majority`,
-    SENDER: 'oldhumblelion@gmail.com',
-    MP: 'Humblelion',
-    REC: 'virgile.dokouvi@gmail.com',
-  },
+
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.

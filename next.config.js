eaa4dd69bc775f04fed4c82bc2f38a5966f0542b
d <virgile.dokouvi@gmail.com>
2022-03-03 13:42:00 -0500

    ignored es build

--- next.config.js
+++ next.config.js
@@ -6,4 +6,9 @@ w=majority`,
     MP: 'Humblelion',
     REC: 'virgile.dokouvi@gmail.com',
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
